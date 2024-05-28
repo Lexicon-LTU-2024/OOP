b5721f2fd38b2cd48699124bcfd7ef251a527cff
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-05-28 13:49:50 +0200

    Castning

--- OOP/Person.cs
+++ OOP/Person.cs
@@ -6,9 +6,34 @@ using System.Threading.Tasks;
 
 namespace OOP
 {
-    internal class Person
+
+    internal interface IPerson
+    {
+        string Name { get; set; }
+        void Do();
+       // void Do2();
+    }
+
+    internal class Person : IPerson
     {
         public string? Name { get; set; } //= string.Empty;
-       
+        public void Do()
+        {
+            Console.WriteLine("Person doing");
+        }
+    }
+
+    internal class Employee : Person
+    {
+        public int Salary { get; set; }
+
+    }
+
+    internal class Admin : Employee
+    {
+        public void ADo()
+        {
+            Console.WriteLine("Admin doing");
+        }
     }
 }
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -5,9 +5,34 @@
         static void Main(string[] args)
         {
 
-            var person = new Person();
-            var nameLength = person.Name?.Length ?? 0;
-            Person person2 = new();
+            Person person1 = new();
+            var nameLength = person1.Name?.Length ?? 0;
+            Admin admin = new Admin();
+            Person person4 = new Employee();
+
+
+            Employee person3 = new Employee();
+
+            List<Person> list = new List<Person>() 
+            { 
+                person1, 
+                admin, 
+                person3 
+            };
+
+            foreach (Person person in list)
+            {
+                //Admin admin2 = (Admin)person;
+                if(person is Admin admin3)
+                {
+                    Admin admin2 = (Admin)person;
+                    admin2.ADo();
+
+                    admin3.ADo();
+                }
+
+
+            }
 
         }
     }
