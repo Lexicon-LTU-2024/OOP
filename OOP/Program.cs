91e95e8f6d19eddea8ee99e532b6438179ef560d
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-05-28 13:24:49 +0200

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person = new Person();
+            var nameLength = person.Name?.Length ?? 0;
+            Person person2 = new();
+
+        }
+    }
+}
